473267bdf0620400e3f776fcd90eee473d92aa00
Tarssito <tarssito@gmail.com>
2021-01-24 19:15:15 -0300

    unit test: create loan test (green)

--- src/main/java/com/tarssito/libraryapi/api/controller/LoanController.java
+++ src/main/java/com/tarssito/libraryapi/api/controller/LoanController.java
@@ -1,4 +1,34 @@
 package com.tarssito.libraryapi.api.controller;
 
+import com.tarssito.libraryapi.api.dto.LoanDTO;
+import com.tarssito.libraryapi.model.entity.Book;
+import com.tarssito.libraryapi.model.entity.Loan;
+import com.tarssito.libraryapi.service.BookService;
+import com.tarssito.libraryapi.service.LoanService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("api/loans")
+@RequiredArgsConstructor
 public class LoanController {
+
+    private final LoanService loanService;
+    private final BookService bookService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Long create(@RequestBody LoanDTO dto) {
+        Book book = bookService.getBookByIsbn(dto.getIsbn()).get();
+        Loan entity = Loan.builder()
+                    .book(book)
+                    .costumer(dto.getCostumer())
+                    .loanDate(LocalDate.now())
+                    .build();
+        Loan loan = loanService.save(entity);
+        return loan.getId();
+    }
 }
--- src/test/java/com/tarssito/libraryapi/api/controller/LoanControllerTest.java
+++ src/test/java/com/tarssito/libraryapi/api/controller/LoanControllerTest.java
@@ -26,8 +26,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import java.time.LocalDate;
 import java.util.Optional;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
@@ -72,6 +71,6 @@ public class LoanControllerTest {
         mockMvc
                 .perform(request)
                 .andExpect(status().isCreated())
-                .andExpect(jsonPath("id").value(1L));
+                .andExpect(content().string("1"));
     }
 }
